e39265378f77f477e5f78c1092b8786d61bf3f23
Geert Jansen <geertj@gmail.com>
2007-11-08 22:20:04 +0000

    Do not put code in __init__.py

--- freeadi/__init__.py
+++ freeadi/__init__.py
@@ -1,10 +1,1 @@
 #
-# This file is part of FreeADI. FreeADI is free software that is made
-# available under the MIT license. Consult the file "LICENSE" that is
-# distributed together with this file for the exact licensing terms.
-#
-# FreeADI is copyright (c) 2007 by the FreeADI authors. See the file
-# "AUTHORS" for a complete overview.
-
-class FreeADIError(Exception):
-    """Base class for FreeADI exceptions."""
