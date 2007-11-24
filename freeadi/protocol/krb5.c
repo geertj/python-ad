40e2fb361c77a552884d76378fe9a2f9fbe8bb7d
Geert Jansen <geertj@gmail.com>
2007-11-24 16:02:14 +0100

    Better error reporting, close ccache and fix gic_keytab().

--- freeadi/protocol/krb5.c
+++ freeadi/protocol/krb5.c
@@ -13,13 +13,13 @@
 
 static PyObject *k5_error;
 
-#define RETURN_ON_ERROR(code) \
+#define RETURN_ON_ERROR(message, code) \
     do if (code != 0) \
     { \
-        const char *message; \
-        message = krb5_get_error_message(ctx, code); \
-        PyErr_Format(k5_error, message); \
-        krb5_free_error_message(ctx, message); \
+        const char *error; \
+        error = krb5_get_error_message(ctx, code); \
+        PyErr_Format(k5_error, "%s: %s", message, error); \
+        krb5_free_error_message(ctx, error); \
         return NULL; \
     } while (0)
 
@@ -40,24 +40,25 @@ k5_get_init_creds_password(PyObject *self, PyObject *args)
 
     /* Initialize parameters. */
     code = krb5_init_context(&ctx);
-    RETURN_ON_ERROR(code);
+    RETURN_ON_ERROR("krb5_init_context()", code);
     code = krb5_parse_name(ctx, name, &principal);
-    RETURN_ON_ERROR(code);
+    RETURN_ON_ERROR("krb5_parse_name()", code);
     krb5_get_init_creds_opt_init(&options);
     memset(&creds, 0, sizeof (creds));
 
     /* Get the credentials. */
     code = krb5_get_init_creds_password(ctx, &creds, principal, password,
                                         NULL, NULL, 0, NULL, &options);
-    RETURN_ON_ERROR(code);
+    RETURN_ON_ERROR("krb5_get_init_creds_password()", code);
 
     /* Store the credential in the credential cache. */
     code = krb5_cc_default(ctx, &ccache);
-    RETURN_ON_ERROR(code);
+    RETURN_ON_ERROR("krb5_cc_default()", code);
     code = krb5_cc_initialize(ctx, ccache, principal);
-    RETURN_ON_ERROR(code);
+    RETURN_ON_ERROR("krb5_cc_initialize()", code);
     code = krb5_cc_store_cred(ctx, ccache, &creds);
-    RETURN_ON_ERROR(code);
+    RETURN_ON_ERROR("krb5_cc_store_creds()", code);
+    krb5_cc_close(ctx, ccache);
 
     Py_INCREF(Py_None);
     return Py_None;
@@ -67,37 +68,50 @@ k5_get_init_creds_password(PyObject *self, PyObject *args)
 static PyObject *
 k5_get_init_creds_keytab(PyObject *self, PyObject *args)
 {
-    char *name, *password;
+    char *name, *ktname;
     krb5_context ctx;
     krb5_error_code code;
+    krb5_keytab keytab;
     krb5_ccache ccache;
     krb5_principal principal;
     krb5_get_init_creds_opt options;
     krb5_creds creds;
 
-    if (!PyArg_ParseTuple(args, "ss", &name, &password))
+    if (!PyArg_ParseTuple(args, "sz", &name, &ktname))
         return NULL;
 
     /* Initialize parameters. */
     code = krb5_init_context(&ctx);
-    RETURN_ON_ERROR(code);
+    RETURN_ON_ERROR("krb5_init_context()", code);
     code = krb5_parse_name(ctx, name, &principal);
-    RETURN_ON_ERROR(code);
+    RETURN_ON_ERROR("krb5_parse_name()", code);
     krb5_get_init_creds_opt_init(&options);
     memset(&creds, 0, sizeof (creds));
 
+    /* Resolve keytab */
+    if (ktname)
+    {
+	code = krb5_kt_resolve(ctx, ktname, &keytab);
+	RETURN_ON_ERROR("krb5_kt_resolve()", code);
+    } else
+    {
+	code = krb5_kt_default(ctx, &keytab);
+	RETURN_ON_ERROR("krb5_kt_resolve()", code);
+    }
+
     /* Get the credentials. */
     code = krb5_get_init_creds_keytab(ctx, &creds, principal,
-                                      0, 0, NULL, &options);
-    RETURN_ON_ERROR(code);
+                                      keytab, 0, NULL, &options);
+    RETURN_ON_ERROR("krb5_get_init_creds_keytab()", code);
 
     /* Store the credential in the credential cache. */
     code = krb5_cc_default(ctx, &ccache);
-    RETURN_ON_ERROR(code);
+    RETURN_ON_ERROR("krb5_cc_default()", code);
     code = krb5_cc_initialize(ctx, ccache, principal);
-    RETURN_ON_ERROR(code);
+    RETURN_ON_ERROR("krb5_cc_initialize()", code);
     code = krb5_cc_store_cred(ctx, ccache, &creds);
-    RETURN_ON_ERROR(code);
+    RETURN_ON_ERROR("krb5_cc_store_creds()", code);
+    krb5_cc_close(ctx, ccache);
 
     Py_INCREF(Py_None);
     return Py_None;
